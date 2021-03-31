cebf030bf5c674325e182456bd8859ceb66b42be
ilhamsetia18 <81663480+ilhamsetia18@users.noreply.github.com>
2021-03-31 09:58:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,32 +9,26 @@ $dbpass     =  $db["pass"];
 $port       =  $db["port"];
 $dbname    =  $db["path"];
 
-// script koneksi php postgree
-$conn = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
- 
-if($conn)
-{
-    echo "Koneksi Berhasil";
-}else
-{
-    echo "Gagal melakukan Koneksi";
+try {
+  $conn = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+  // sql to create table
+  $sql = "CREATE TABLE MyGuests (
+  id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  firstname VARCHAR(30) NOT NULL,
+  lastname VARCHAR(30) NOT NULL,
+  email VARCHAR(50),
+  reg_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+  )";
+
+  // use exec() because no results are returned
+  $conn->exec($sql);
+  echo "Table MyGuests created successfully";
+} catch(PDOException $e) {
+  echo $sql . "<br>" . $e->getMessage();
 }
 
-  // set the PDO error mode to exception
-   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-   // sql to create table
-   $sql = "CREATE TABLE tbl_pegawai (
-   id_pegawai INT(5) NOT NULL AUTO_INCREMENT,
-   nama_pegawai VARCHAR(20) DEFAULT NULL,
-   jenis_kelamin CHAR(1) DEFAULT NULL,
-   gaji DECIMAL(10,0) DEFAULT NULL,
-   alamat VARCHAR(20) DEFAULT NULL,
-   departemen VARCHAR(5) DEFAULT NULL,
-   PRIMARY KEY (id_pegawai)
-   )";
-
-   // use exec() because no results are returned
-   $conn->exec($sql);
-   echo "Table tbl_pegawai berhasil di buat";
+$conn = null;
 ?>
