4d3fe36a0b616880190a8ab4c301c5f8606c82ca
ilhamsetia18 <81663480+ilhamsetia18@users.noreply.github.com>
2021-03-31 09:50:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -10,13 +10,31 @@ $port       =  $db["port"];
 $dbname    =  $db["path"];
 
 // script koneksi php postgree
-$link = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
+$conn = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
  
-if($link)
+if($conn)
 {
     echo "Koneksi Berhasil";
 }else
 {
     echo "Gagal melakukan Koneksi";
 }
+
+  // set the PDO error mode to exception
+   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+   // sql to create table
+   $sql = "CREATE TABLE tbl_pegawai (
+   id_pegawai INT(5) NOT NULL AUTO_INCREMENT,
+   nama_pegawai VARCHAR(20) DEFAULT NULL,
+   jenis_kelamin CHAR(1) DEFAULT NULL,
+   gaji DECIMAL(10,0) DEFAULT NULL,
+   alamat VARCHAR(20) DEFAULT NULL,
+   departemen VARCHAR(5) DEFAULT NULL,
+   PRIMARY KEY (id_pegawai)
+   )";
+
+   // use exec() because no results are returned
+   $conn->exec($sql);
+   echo "Table tbl_pegawai berhasil di buat";
 ?>
