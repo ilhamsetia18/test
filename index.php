6400fe9a0f450b446c7a38865aba0a2f7cb0f377
ilhamsetia18 <81663480+ilhamsetia18@users.noreply.github.com>
2021-03-31 09:43:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -2,4 +2,21 @@
 $db = parse_url(getenv("DATABASE_URL"));
 $db["path"] = ltrim($db["path"], "/");
 
-var_dump($db);
+// konfigurasi koneksi
+$host       =  $db["host"];
+$dbuser     =  $db["user"];
+$dbpass     =  $db["pass"];
+$port       =  $db["port"];
+$dbname    =  $db["path"];
+
+// script koneksi php postgree
+$link = new PDO("pgsql:dbname=$dbname;host=$host", $dbuser, $dbpass); 
+ 
+if($link)
+{
+    echo "Koneksi Berhasil";
+}else
+{
+    echo "Gagal melakukan Koneksi";
+}
+?>
